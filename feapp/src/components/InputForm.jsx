ed1449d8682981df94733175305496fb5f04f25a
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-12-05 18:36:27 +0700

    Update Form

--- feapp/src/components/InputForm.jsx
+++ feapp/src/components/InputForm.jsx
@@ -8,6 +8,10 @@ const InputForm = () => {
   const [inputValue, setInputValue] = useState('');
   const navigate = useNavigate();
 
+  const goToAllPlaces = () => {
+    navigate('/allplaces');
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -23,18 +27,20 @@ const InputForm = () => {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col items-center p-4 bg-white border border-gray-200 rounded-lg">
-      <input
+      {/* <input
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Masukkan deskripsi yang diinginkan"
         className="p-3 mb-4 w-full text-lg border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-colors"
-      />
+      /> */}
       <button
-        type="submit"
+        type="Semua Tempat"
+        onClick={goToAllPlaces}
         className="w-full px-6 py-3 bg-blue-600 text-white text-lg rounded-lg hover:bg-blue-700 transition-colors"
+        
       >
-        Submit
+        Semua Tempat
       </button>
     </form>
   );
